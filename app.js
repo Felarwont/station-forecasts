69ff762c65806725fd77b1c2d0443b878151e8f3
Karimov <karimov29rushan@gmail.com>
2026-09-21 11:07:53 +0500

    Add: clear messages for users

--- app.js
+++ app.js
@@ -4,23 +4,23 @@ const stops = [
   {
     id: 10116,
     title: 'Цирк',
-    description: 'в сторону Площадь Мира'
+    direction: 'в сторону Площадь Мира'
   },
   {
     id: 10017,
     title: 'Юность',
-    description: 'в сторону Проспект Карла Маркса 115'
+    direction: 'в сторону Проспект Карла Маркса 115'
   },
   {
     id: 10053,
     title: 'Улица Труда',
-    description: 'в сторону Улица Бориса Ручьева'
+    direction: 'в сторону Улица Бориса Ручьева'
   }
 ];
 
-async function getStopInfo(stopId) {
-
-}
+// TODO: Поиск остановок
+// async function getStopInfo(stopId) {
+// }
 
 async function getRoutes(stopId) {
   const response = await fetch(API,
@@ -44,13 +44,13 @@ async function getRoutes(stopId) {
   );
 
   if (!response.ok) {
-    throw new Error('Error, status:', response.status);
+    throw new Error('HTTP Error, status:', response.status);
   }
 
   const json = await response.json();
 
   if (json.r !== 'ok') {
-    console.error('API error:', json)
+    throw new Error('API error:', json);
   }
 
   return json.data
@@ -59,29 +59,38 @@ async function getRoutes(stopId) {
       name: route.routeShortName,
       destination: route.whereGo,
       time: Math.floor(route.arrivalTimeInSec / 60) || "<1"
-    }))
+    }));
 }
 
 async function main() {
   const app = document.getElementById('app');
+
   for (const stop of stops) {
     const station = document.createElement('div');
     station.className = 'station';
 
     let html = 
       `<div class=station-title>${stop.title}</div>
-      <div class=station-desc>${stop.description}</div>`
+      <div class=station-dir>${stop.direction}</div>`;
 
-    const routes = await getRoutes(stop.id);
-
-    for (const route of routes) {
-      html +=
-        `<div class=row>
-          <div class=route>${route.name}</div>
-          <div class=dest>${route.destination}</div>
-          <div class=time>${route.time}м</div>
-        </div>`
+    try {
+      const routes = await getRoutes(stop.id);
+      if (!routes.length) {
+        html += '<div style=color:#555>Трамваев нет</div>';
+      } else {
+        routes.forEach(route => (
+          html +=
+            `<div class=row>
+              <div class=route>${route.name}</div>
+              <div class=dest>${route.destination}</div>
+              <div class=time>${route.time}м</div>
+            </div>`
+        ));
+      }
+    } catch (Error) {
+      html += '<div style=color:#555>Ошибка получения данных...</div>';
     }
+
     station.innerHTML = html
     app.appendChild(station)
 
